8fa36921c7c4e88d78284b5bba0c710cfa352a66
IG <ifediorachukwu@yahoo.com>
2023-12-12 15:52:11 +0100

    v0.0.7

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,18 @@
 import React from "react";
-import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { Layout, Menu } from "antd";
 import { Link } from "react-router-dom";
 const { Header } = Layout;
 
 export const NavBar = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
   return (
-    <Header className="h-[80px] bg-white flex justify-between  items-center ">
+    <Header className="h-[80px] bg-white flex justify-between items-center ">
       <div className="demo-logo h-full ">
-        <img
-          className="h-full object-contain "
-          src="/images/logo.jpeg"
-          alt=""
-        />
+        <img className="h-full object-contain " src="/images/logo.jpeg" />
       </div>
       <Menu
-        theme="light"
         mode="horizontal"
-        className="min-w-0 justify-end flex-1" //Width not responsive
-        defaultSelectedKeys={["1"]}
+        className="min-w-0 justify-end flex-auto" //Width not fully responsive
+        defaultSelectedKeys="1"
       >
         <Menu.Item key={1}>
           <Link to={"/"}>Home</Link>
