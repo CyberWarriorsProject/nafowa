1bdf278af1bcdf107d745bdf89468b4c9b650032
ogunTee <adeniyioguntamu@gmail.com>
2023-12-14 13:44:01 +0100

    about v0.0.2

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { MdRocketLaunch } from "react-icons/md";
 import HeroOverlay from "./HeroOverlay";
+import Vision from "./Vision";
 
 export const About = () => {
   return (
-    <div>
+    <>
       <HeroOverlay />
-    </div>
+      <Vision />
+    </>
   );
 };
--- src/pages/about/HeroOverlay.jsx
+++ src/pages/about/HeroOverlay.jsx
@@ -7,8 +7,10 @@ export default function () {
         src="https://1.bp.blogspot.com/-akk1pth4xXI/X-JTa3TmmFI/AAAAAAACDAw/w6jayo_FAWgUrtGXKV4RmuopFDcFUMobwCNcBGAsYHQ/s16000/1.jpg"
         className="w-full h-full object-cover"
       />
-      <div className="absolute top-0 bottom-0 left-0 right-0 bg-red-500 h-[200px] w-full">
-        <h1>About Us</h1>
+      <div className="absolute top-0 bottom-0 left-0 right-0 bg-nafowaBlue bg-opacity-50 h-[200px] w-full flex items-center px-4">
+        <h1 className="text-[30px] font-sans font-extrabold text-gray-800 ">
+          About Us
+        </h1>
       </div>
     </div>
   );
