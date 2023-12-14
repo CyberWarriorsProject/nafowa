021d9ee0dbf9e9bbf42992a546022f7ae65b7cfc
IG <ifediorachukwu@yahoo.com>
2023-12-14 15:33:02 +0100

    App updated - duplicate about component resolved

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import { MainLayout } from "./layouts/MainLayout";
 import { NotFound } from "./components/NotFound";
 import { Home } from "./pages/home/Home";
-import { About } from "./pages/About";
 import { Services } from "./pages/Services";
 import { Contact } from "./pages/Contact";
 import { About } from "./pages/about/About";
