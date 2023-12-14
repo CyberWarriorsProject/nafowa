a9b127ba0c0d0c020f0a9575eb204e318f798590
Samuel Adebayo <repzysam@gmail.com>
2023-12-14 15:00:34 +0100

    home page v0.08

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { MainLayout } from "./layouts/MainLayout";
 import { NotFound } from "./components/NotFound";
-import { Home } from "./pages/Home";
+import { Home } from "./pages/home/Home";
 import { About } from "./pages/About";
 import { Services } from "./pages/Services";
 import { Contact } from "./pages/Contact";
